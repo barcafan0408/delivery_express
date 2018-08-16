88e6fce06a875ff52328b59eb59f03c334aa0c21
Alex Bezpalko <a.bespalko88@gmail.com>
2018-08-16 15:30:03 +0300

    added nodemon

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: 'localhost',
       name: 'delivery_db',
       user: 'root',
-      password: 'barcafan1',
+      password: 'barcafan1'
     },
   },
   production: {
